596f2bc8e9257714318b37b6e5abd7eb84cd47f2
Elan Ruusamäe <glen@delfi.ee>
2015-12-02 17:52:35 +0200

    update url to this plugin

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 	author='Elan Ruusamäe',
 	author_email='glen@pld-linux.org',
 	description='Automatically create links for Eventum issue ID-s.',
-	url='https://github.com/eventum/eventum',
+	url='https://github.com/eventum/trac-plugin-eventum',
 	license='BSD-like',
 	packages=['trac.eventum'],
 	entry_points = {'trac.plugins': ['trac.eventum = trac.eventum']}
